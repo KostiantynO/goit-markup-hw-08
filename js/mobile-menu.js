802a33d4430914efe42985e45c0784c44e6d097e
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-08-07 17:04:00 +0300

    adds 1s animations and transitions for features__heading

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,38 @@
-(() => {
+(function () {
   const refsMobile = {
-    mobileMenu: document.querySelector("[data-menu]"),
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    htmlAndBody: document.querySelectorAll("[data-no-scroll]"),
   };
 
-  function toggleMenu() {
-    refsMobile.mobileMenu.classList.toggle("is-open");
+  function toggleModal() {
+    refsMobile.menu.classList.toggle("is-hidden"),
+      refsMobile.htmlAndBody[0].classList.toggle("no-scroll"),
+      refsMobile.htmlAndBody[1].classList.toggle("no-scroll");
   }
 
-  refsMobile.openMenuBtn.addEventListener("click", toggleMenu);
-  refsMobile.closeMenuBtn.addEventListener("click", toggleMenu);
+  function focusLog(targetElem = refsMobile.openMenuBtn) {
+    /* Чарівництво */
+    setTimeout(() => {
+      targetElem.focus();
+      isFocused = document.activeElement === targetElem;
+      console.log(`${targetElem.className} isFocused: ${isFocused}`);
+    }, 250);
+  }
+
+  refsMobile.openMenuBtn.addEventListener("click", function () {
+    refsMobile.menu.classList.contains("is-hidden") && (toggleModal(), focusLog(refsMobile.modal));
+  }),
+    refsMobile.closeMenuBtn.addEventListener("click", event => {
+      toggleModal(), focusLog();
+    }),
+    refsMobile.menu.addEventListener("keyup", event => {
+      (event.which === 27 || event.key === "Escape") &&
+        !refsMobile.menu.classList.contains("is-hidden") &&
+        (toggleModal(), focusLog());
+    }),
+    refsMobile.menu.addEventListener("mousedown", event => {
+      event.target.matches("[data-menu]") && (toggleModal(), focusLog());
+    });
 })();
